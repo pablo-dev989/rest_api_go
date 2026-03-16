d23b7b18e0c15957ac72259d7ef5a94d7e214424
pablo <261613295+pablo-dev989@users.noreply.github.com>
2026-03-16 15:20:51 -0300

    Add SQL MARIA DB

---
+++ internal/repository/sqlconnect/sqlconfig.go
@@ -0,0 +1,35 @@
+package sqlconnect
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectDb() (*sql.DB, error) {
+	fmt.Println("Trying to connect MariaDB.")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("HOST")
+	dbport := os.Getenv("DB_PORT")
+
+	// connectionString := "root:sasa123@tcp(host.docker.internal:3306)/" + dbname
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, dbport, dbname)
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		// panic(err)
+		return nil, err
+	}
+
+	// fmt.Println(connectionString)
+	fmt.Println("Connected to MariaDB.")
+	return db, nil
+}
